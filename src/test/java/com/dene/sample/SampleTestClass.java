fc5d13d488e28c719824c06de72074118c3f04ba
Dene <denemcardle@gmail.com>
2018-11-13 15:50:06 +0000

    Add null test, wetting feet with git

---
+++ src/test/java/com/dene/sample/SampleTestClass.java
@@ -0,0 +1,14 @@
+package com.dene.sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTestClass {
+	
+	@Test
+	public void test(){
+		assertTrue(true);
+	}
+
+}
